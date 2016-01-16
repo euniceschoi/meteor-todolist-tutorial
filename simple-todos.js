a2dccc28a59d377a8bc03152442d060c36311c50
euniceschoi <euniceschoi93@gmail.com>
2016-01-15 17:41:34 -0800

    add event handler for hide complete tasks option

--- simple-todos.js
+++ simple-todos.js
@@ -20,6 +20,9 @@ if (Meteor.isClient) {
       });
 
       event.target.text.value = "";
+    },
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
     }
   });
 
